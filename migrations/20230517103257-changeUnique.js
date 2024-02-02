201dba40fc6b1b6f9825523fbd202f9e55b93be4
shuhratbek1999 <shuhratbekzokirjonov1119@gmail.com>
2024-02-02 09:15:55 +0500

    backs

--- migrations/20230517103257-changeUnique.js
+++ migrations/20230517103257-changeUnique.js
@@ -2,33 +2,30 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     const transaction = await queryInterface.sequelize.transaction();
-       try{
-        await queryInterface.sequelize.query(
-          'ALTER TABLE category DROP INDEX IF EXISTS page_id',
-          {
-            type: Sequelize.QueryTypes.UPDATE
-          }
-        );
-        transaction.commit()
-       }
-       catch(err){
-         transaction.rollback();
-         throw err;
-       }
-      },
+    try {
+      await queryInterface.sequelize.query(
+        "ALTER TABLE `category` DROP INDEX IF EXISTS page_id",
+        { transaction } // Transaction ni qo'shing
+      );
+      await transaction.commit(); // Transactionni qo'shing
+    } catch (err) {
+      await transaction.rollback(); // Transactionni qo'shing
+      throw err;
+    }
+  },
 
-      async down (queryInterface, Sequelize) {
-        const transaction = await queryInterface.sequelize.transaction();
-        try {
-          await queryInterface.changeColumn('category', 'page_id', {
-            unique: true
-         })
-          transaction.commit();
-        } catch (errors) {
-          transaction.rollback();
-          throw errors;
-        }
-      }
+  async down(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.changeColumn('category', 'page_id', {
+        unique: true,
+      }, { transaction }); // Transactionni qo'shing
+      await transaction.commit(); // Transactionni qo'shing
+    } catch (errors) {
+      await transaction.rollback(); // Transactionni qo'shing
+      throw errors;
+    }
+  }
 };
